a0ad32688396a5f0fcde08afca141ca07691766c
RichardDolhy <dolhyriso@gmail.com>
2025-12-11 09:39:49 +0100

    removeing backend

--- backend/backend-django/api/utils/extract_mfcc.py
+++
@@ -1,8 +0,0 @@
-import librosa
-import numpy as np
-from config import SAMPLE_RATE, N_MFCC
-
-def extract_mfcc_from_song(path):
-    y, sr = librosa.load(path, sr=SAMPLE_RATE)
-    mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=N_MFCC)
-    return mfcc[..., np.newaxis]
